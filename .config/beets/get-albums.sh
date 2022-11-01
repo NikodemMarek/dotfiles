c73094c09be3ff48f6a0e789f9bdf8c247afb0b9
Nikodem <nikodemmarek11@gmail.com>
2022-11-01 22:54:59 +0100

    feat: beets music manager

---
+++ .config/beets/get-albums.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for url in "$@"
+do
+  youtube-dl -f "bestaudio/best" -ciw -o "./.tmp/%(title)s.%(ext)s" -v --extract-audio --audio-format flac --audio-quality 0 --yes-playlist "$url"
+
+  printf '\nA' | beet import -w -a -g -m "./.tmp"
+done
