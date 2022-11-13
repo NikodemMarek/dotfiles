cdfaa81987826139c7301b72ef1eff55f8981da7
nikodem <nikodemmarek11@gmail.com>
2022-11-13 18:22:37 +0100

    feat(beets): folders rearrangement and useful plugins

--- .config/beets/get-albums.sh
+++ .config/beets/get-albums.sh
@@ -2,7 +2,7 @@
 
 for url in "$@"
 do
-  youtube-dl -f "bestaudio/best" -ciw -o "./.tmp/%(title)s.%(ext)s" -v --extract-audio --audio-format flac --audio-quality 0 --yes-playlist "$url"
+  youtube-dl -f "bestaudio/best" -ciw -o "$HOME/.config/beets/.tmp/%(title)s.%(ext)s" -v --extract-audio --audio-format flac --audio-quality 0 --yes-playlist "$url"
 
   printf '\nA' | beet import -w -a -g -m "./.tmp"
 done
