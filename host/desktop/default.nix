c345c3d05a4aa1aaddc419cd49c12fefeaf93077
nikodem <nikodemmarek11@gmail.com>
2024-03-06 14:34:36 +0000

    feat(host): docker group for school

--- host/desktop/default.nix
+++ host/desktop/default.nix
@@ -1,51 +1,65 @@
 {
-    users = [
-        {
-            username = "nikodem";
-            groups = [ "wheel" "networkmanager" "docker" ];
-        }
-        {
-            username = "school";
-            groups = [ "networkmanager" ];
-        }
-        {
-            username = "fun";
-            groups = [ "wheel" "networkmanager" "docker" ];
-        }
-        {
-            username = "rustchain";
-            groups = [ "networkmanager" "docker" ];
-        }
-    ];
+  users = [
+    {
+      username = "nikodem";
+      groups = ["wheel" "networkmanager" "docker"];
+    }
+    {
+      username = "school";
+      groups = ["networkmanager" "docker" "music"];
+    }
+    {
+      username = "fun";
+      groups = ["wheel" "networkmanager" "docker"];
+    }
+    {
+      username = "rustchain";
+      groups = ["networkmanager" "docker"];
+    }
+  ];
 
-    settings = {
-        device = "desktop";
-        resolution = { width = 2560; height = 1440; };
+  settings = {
+    device = "desktop";
+    resolution = {
+      width = 2560;
+      height = 1440;
+    };
+  };
+
+  module = {
+    inputs,
+    outputs,
+    lib,
+    config,
+    pkgs,
+    ...
+  }: {
+    fileSystems = {
+      "/".options = ["compress=lzo"];
+      "/home".options = ["compress=lzo"];
+      "/nix".options = ["compress=lzo" "noatime"];
     };
 
-    module = { inputs, outputs, lib, config, pkgs, ... }: {
-      fileSystems = {
-        "/".options = [ "compress=lzo" ];
-        "/home".options = [ "compress=lzo" ];
-        "/nix".options = [ "compress=lzo" "noatime" ];
-      };
-
-      swapDevices = [{
+    swapDevices = [
+      {
         device = "/swapfile";
         size = 38 * 1024;
-      }];
-      boot.kernelParams = [ "resume_offset=25928960" ];
-      boot.resumeDevice = "/dev/disk/by-uuid/fc60a589-f4e6-4f9e-b149-b372221e824b";
-      security.protectKernelImage = false;
-
-      boot.extraModulePackages = [
-        config.boot.kernelPackages.rtl8821au
-      ];
-
-      environment.systemPackages = with pkgs; [
-        mesa
-        linuxKernel.packages.linux_6_1.rtl8821au
-      ];
-      hardware.opengl.driSupport32Bit = true;
+      }
+    ];
+    boot = {
+      kernelParams = ["resume_offset=25928960"];
+      resumeDevice = "/dev/disk/by-uuid/fc60a589-f4e6-4f9e-b149-b372221e824b";
     };
+    security.protectKernelImage = false;
+
+    boot.extraModulePackages = [
+      config.boot.kernelPackages.rtl8821au
+    ];
+
+    environment.systemPackages = with pkgs; [
+      mesa
+      linuxKernel.packages.linux_6_1.rtl8821au
+    ];
+    hardware.opengl.driSupport32Bit = true;
+  };
 }
--- host/laptop/default.nix
+++ host/laptop/default.nix
@@ -1,57 +1,71 @@
 {
-    users = [
-        {
-            username = "nikodem";
-            groups = [ "wheel" "networkmanager" "docker" "music" ];
-        }
-        {
-            username = "school";
-            groups = [ "networkmanager" "docker" "music" ];
-        }
-        {
-            username = "fun";
-            groups = [ "wheel" "networkmanager" "docker" "music" ];
-        }
-        {
-            username = "rustchain";
-            groups = [ "networkmanager" "docker" ];
-        }
+  users = [
+    {
+      username = "nikodem";
+      groups = ["wheel" "networkmanager" "docker" "music"];
+    }
+    {
+      username = "school";
+      groups = ["networkmanager" "docker" "music"];
+    }
+    {
+      username = "fun";
+      groups = ["wheel" "networkmanager" "docker" "music"];
+    }
+    {
+      username = "rustchain";
+      groups = ["networkmanager" "docker"];
+    }
+  ];
+
+  settings = {
+    device = "laptop";
+    resolution = {
+      width = 1920;
+      height = 1080;
+    };
+  };
+
+  module = {
+    inputs,
+    outputs,
+    lib,
+    config,
+    pkgs,
+    ...
+  }: {
+    imports = [
+      ../music.nix
     ];
 
-    settings = {
-        device = "laptop";
-        resolution = { width = 1920; height = 1080; };
+    fileSystems = {
+      "/".options = ["compress=lzo"];
+      "/home".options = ["compress=lzo"];
+      "/nix".options = ["compress=lzo" "noatime"];
     };
 
-    module = { inputs, outputs, lib, config, pkgs, ... }: {
-      imports = [
-        ../music.nix
-      ];
-        
-      fileSystems = {
-        "/".options = [ "compress=lzo" ];
-        "/home".options = [ "compress=lzo" ];
-        "/nix".options = [ "compress=lzo" "noatime" ];
-      };
-
-      swapDevices = [{
+    swapDevices = [
+      {
         device = "/swapfile";
         size = 15 * 1024;
-      }];
-      boot.kernelParams = [ "resume_offset=25442627" ];
-      boot.resumeDevice = "/dev/disk/by-uuid/b537105a-8de8-4724-807c-039e5b23f3bb";
-
-      environment.systemPackages = with pkgs; [
-        mesa
-      ];
-
-      hardware.bluetooth = {
-        enable = true;
-        powerOnBoot = true;
-      };
-      hardware.opengl.driSupport32Bit = true;
-
-      services.thermald.enable = true;
-      services.auto-cpufreq.enable = true;
+      }
+    ];
+    boot = {
+      kernelParams = ["resume_offset=25442627"];
+      resumeDevice = "/dev/disk/by-uuid/b537105a-8de8-4724-807c-039e5b23f3bb";
     };
+
+    environment.systemPackages = with pkgs; [
+      mesa
+    ];
+
+    hardware.bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+    hardware.opengl.driSupport32Bit = true;
+
+    services.thermald.enable = true;
+    services.auto-cpufreq.enable = true;
+  };
 }
