3f67e24f1ca5d318621b01d5a4724505ba885b68
nikodem <nikodemmarek11@gmail.com>
2024-04-05 12:46:55 +0000

    feat(networking): open ports 5xxx and ssh port 22

--- host/modules/networking.nix
+++ host/modules/networking.nix
@@ -1,8 +1,23 @@
 {hostname, ...}: {
   networking = {
     hostName = hostname;
-    firewall.enable = true;
     nftables.enable = true;
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [22];
+      allowedTCPPortRanges = [
+        {
+          from = 5000;
+          to = 5999;
+        }
+      ];
+      allowedUDPPortRanges = [
+        {
+          from = 5000;
+          to = 5999;
+        }
+      ];
+    };
 
     networkmanager = {
       enable = true;
