0523bdf6426c0ba1099706c2b9413f21f08fc670
nikodem <nikodemmarek11@gmail.com>
2024-05-01 07:47:32 +0000

    feat(tools): more default tools

--- home/modules/tools.nix
+++ home/modules/tools.nix
@@ -1,7 +1,15 @@
-{ config, pkgs, lib, ... }: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   home.packages = with pkgs; [
-    zip unzip
+    zip
+    unzip
     bottom
     xxd
+    fd
+    ripgrep
   ];
 }
