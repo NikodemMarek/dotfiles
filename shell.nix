61c8d67e1a8f8d7cab1f5b0e66907b127f9ae565
nikodem <nikodemmarek11@gmail.com>
2024-06-19 18:55:17 +0000

    feat: updated scripts

--- shell.nix
+++ shell.nix
@@ -16,20 +16,20 @@
     {
       name = "ssh-new-user-key";
       command = ''
-        ssh-keygen -f ./host/$1/users/$2/id_ed25519 -t ed25519 -C $2@$1
+        ssh-keygen -f ./setups/$1/users/$2/id_ed25519 -t ed25519 -C $2@$1
 
         printf "\nage key:\n"
-        ssh-to-age < ./host/$1/users/$2/id_ed25519.pub
+        ssh-to-age < ./setups/$1/users/$2/id_ed25519.pub
       '';
       description = "[host user] create a new user key";
     }
     {
       name = "ssh-new-host-key";
       command = ''
-        ssh-keygen -f ./host/$1/ssh_host_ed25519_key -t ed25519 -C root@$1
+        ssh-keygen -f ./setups/$1/ssh_host_ed25519_key -t ed25519 -C root@$1
 
         printf "\nadd this key to .sops.yaml\n"
-        ssh-to-age < ./host/$1/ssh_host_ed25519_key.pub
+        ssh-to-age < ./setups/$1/ssh_host_ed25519_key.pub
         printf "\nand run sops-update-keys\n"
       '';
       description = "[host] create a new host key";
@@ -66,7 +66,7 @@
 
         install -d -m755 "$temp/persist/etc/ssh"
 
-        cat ./host/$1/ssh_host_ed25519_key > "$temp/persist/etc/ssh/ssh_host_ed25519_key"
+        cat ./setups/$1/ssh_host_ed25519_key > "$temp/persist/etc/ssh/ssh_host_ed25519_key"
 
         chmod 600 "$temp/persist/etc/ssh/ssh_host_ed25519_key"
 
