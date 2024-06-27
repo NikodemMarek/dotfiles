53aa306b30c8d80e2ec78cc0efb1db6d7d3b41a0
nikodem <nikodemmarek11@gmail.com>
2024-06-27 19:06:55 +0000

    feat: networking and desktop secrets

--- host/networking.nix
+++ host/networking.nix
@@ -1,16 +1,28 @@
-{
+{config, ...}: {
   systemd.network = {
     enable = true;
-    networks."default.network" = {
-      matchConfig.Name = "en*";
-      networkConfig = {
-        Description = "default ethernet";
-        DHCP = "yes";
+    networks = {
+      "10-wired-default" = {
+        matchConfig.Name = "en*";
+        networkConfig.DHCP = "yes";
       };
     };
   };
 
   networking = {
+    wireless = {
+      userControlled.enable = true;
+      networks = let
+        # FIXME: Temporary solution that only works if secrets are already present on the host.
+        readIfExists = path:
+          if builtins.pathExists path
+          then builtins.readFile path
+          else null;
+      in {
+        "Meshki56".psk = readIfExists config.sops.secrets."networks/Meshki56/psk";
+      };
+    };
+
     nftables.enable = true;
     firewall = {
       enable = true;
@@ -27,11 +39,6 @@
           to = 5999;
         }
       ];
-    };
-
-    networkmanager = {
-      enable = true;
-      dns = "systemd-resolved";
     };
   };
 }
