ee67c115b8652bc4cea66ab2e1fb0eb403ff32a5
nm1 <nikodem.marek@softnet.com.pl>
2024-07-26 10:17:26 +0200

    fix(anysync): pinned version

--- pkgs/anysync.nix
+++ pkgs/anysync.nix
@@ -9,7 +9,7 @@ rustPlatform.buildRustPackage rec {
   src = pkgs.fetchFromGitHub {
     owner = "NikodemMarek";
     repo = "anysync";
-    rev = "main";
+    rev = "59fd374d8bf72f092325e5e2a224d276710be24c";
     sha256 = "sha256-+uhEwQ+/OHCSGF+9x6Fdf2EWvdHc1SXEB/aTJc4y3UQ=";
   };
 
