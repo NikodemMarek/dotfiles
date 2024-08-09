5e032aad7ece140e1934ceae11b3286caac001c1
nm1 <nikodem.marek@softnet.com.pl>
2024-08-09 07:40:41 +0200

    fix(docker): docker interfaces ignored by networkd

--- host/docker.nix
+++ host/docker.nix
@@ -7,4 +7,19 @@
       setSocketVariable = true;
     };
   };
+
+  systemd.network.networks = {
+    "0-docker0" = {
+      matchConfig.Name = "docker0";
+      linkConfig = {
+        Unmanaged = true;
+      };
+    };
+    "0-docker-br" = {
+      matchConfig.Name = "br-*";
+      linkConfig = {
+        Unmanaged = true;
+      };
+    };
+  };
 }
