ca29e5b0664c2938a8fd4ed94ba712805c72b54f
nm1 <nikodem.marek@softnet.com.pl>
2024-08-19 08:50:10 +0200

    feat(wezterm): better navigation

--- home/common/wezterm.nix
+++ home/common/wezterm.nix
@@ -10,29 +10,76 @@
   programs.wezterm = {
     enable = true;
     package = inputs.wezterm.packages.${pkgs.system}.default;
-    extraConfig= ''
-    return {
-        enable_wayland = false,
-        window_close_confirmation = "NeverPrompt",
+    extraConfig =
+      /*
+      lua
+      */
+      ''
+        return {
+            enable_wayland = false,
+            window_close_confirmation = "NeverPrompt",
 
-        keys = {
-            {
-                key = 'Tab',
-                mods = 'ALT',
-                action = wezterm.action.ActivateTabRelative(1),
+            keys = {
+                {
+                    key = 'Tab',
+                    mods = 'ALT',
+                    action = wezterm.action.ActivateTabRelative(1),
+                },
+                {
+                    key = 'Tab',
+                    mods = 'ALT|SHIFT',
+                    action = wezterm.action.ActivateTabRelative(-1),
+                },
+                {
+                    key = 'Enter',
+                    mods = 'ALT',
+                    action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+                },
+                {
+                    key = 'w',
+                    mods = 'ALT',
+                    action = wezterm.action.CloseCurrentTab { confirm = false },
+                },
+
+                {
+                    key = 'v',
+                    mods = 'ALT',
+                    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+                },
+                {
+                    key = 's',
+                    mods = 'ALT',
+                    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+                },
+
+                {
+                    key = 'l',
+                    mods = 'ALT',
+                    action = wezterm.action.ActivatePaneDirection 'Right',
+                },
+                {
+                    key = 'h',
+                    mods = 'ALT',
+                    action = wezterm.action.ActivatePaneDirection 'Left',
+                },
+                {
+                    key = 'j',
+                    mods = 'ALT',
+                    action = wezterm.action.ActivatePaneDirection 'Down',
+                },
+                {
+                    key = 'k',
+                    mods = 'ALT',
+                    action = wezterm.action.ActivatePaneDirection 'Up',
+                },
+
+                {
+                    key = 'd',
+                    mods = 'ALT',
+                    action = wezterm.action.CloseCurrentPane { confirm = true },
+                },
             },
-            {
-                key = 'Tab',
-                mods = 'ALT|SHIFT',
-                action = wezterm.action.ActivateTabRelative(-1),
-            },
-            {
-                key = 'Enter',
-                mods = 'ALT',
-                action = wezterm.action.SpawnTab('CurrentPaneDomain'),
-            },
-        },
-    }
-    '';
+        }
+      '';
   };
 }
