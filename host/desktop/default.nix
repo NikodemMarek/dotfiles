c42e21dc6252f7a7ff447dec0ba94fd1da0fe9b2
NikodemMarek <nikodemmarek11@gmail.com>
2024-11-30 14:14:28 +0100

    fix(syncthing): it still does not work, but its trying

--- host/desktop/default.nix
+++ host/desktop/default.nix
@@ -62,39 +62,32 @@
     linuxKernel.packages.linux_6_1.rtl8821au
   ];
 
-  services = {
-    music = {
-      enable = true;
-      anysync = true;
-      persistent = true;
-    };
-    # TODO: Migrate to home-manager when it gets support
-    syncthing = {
-      enable = true;
-      openDefaultPorts = false;
-      key = config.sops.secrets."syncthing/key".path;
-      cert = config.sops.secrets."syncthing/cert".path;
-      overrideDevices = true;
-      overrideFolders = true;
-      user = "nikodem";
-      group = "users";
-      dataDir = "${config.users.users.nikodem.home}/.local/share/syncthing";
-      configDir = "${config.users.users.nikodem.home}/.config/syncthing";
-      settings = {
-        devices = {
-          "pixel-6a".id = "L7IXLIC-DU3D4MJ-OVOXKBW-D5M2AW5-3JWOFF4-X43JUPT-JN3XWZL-2FNL6QT";
-          "tablet".id = "SCFNK0Z-UDF56C2-26ZS36D-A2PNTWO-K06IC55-0QGWCTN-MLNQBND-PXD8JQX";
-        };
-        folders = {
-          "obsidian" = {
-            path = "${config.users.users.nikodem.home}/vaults/main";
-            devices = ["pixel-6a" "tablet"];
-            copyOwnershipFromParent = true;
-            versioning = {
-              type = "simple";
-              params = {
-                keep = "5";
-              };
+  # TODO: Migrate to home-manager when it gets support
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = false;
+    key = config.sops.secrets."syncthing/key".path;
+    cert = config.sops.secrets."syncthing/cert".path;
+    overrideDevices = true;
+    overrideFolders = true;
+    user = "nikodem";
+    group = "users";
+    dataDir = "${config.users.users.nikodem.home}/.local/share/syncthing";
+    configDir = "${config.users.users.nikodem.home}/.config/syncthing";
+    settings = {
+      devices = {
+        "pixel-6a".id = "L7IXLIC-DU3D4MJ-OVOXKBW-D5M2AW5-3JWOFF4-X43JUPT-JN3XWZL-2FNL6QT";
+        "tablet".id = "SCFNK0Z-UDF56C2-26ZS36D-A2PNTWO-K06IC55-0QGWCTN-MLNQBND-PXD8JQX";
+      };
+      folders = {
+        "obsidian" = {
+          path = "${config.users.users.nikodem.home}/vaults/main";
+          devices = ["pixel-6a" "tablet"];
+          copyOwnershipFromParent = true;
+          versioning = {
+            type = "simple";
+            params = {
+              keep = "5";
             };
           };
         };
--- host/laptop/default.nix
+++ host/laptop/default.nix
@@ -15,6 +15,7 @@
     ../features/hyprland.nix
     ../features/docker.nix
     ../features/dnscrypt-proxy2.nix
+    ../features/syncthing.nix
 
     ../features/battery-saver.nix
     ../features/bluetooth.nix
@@ -44,6 +45,38 @@
       hashedPasswordFile = config.sops.secrets."users/fun/password".path;
       extraGroups = ["wheel"];
       shell = pkgs.fish;
+    };
+  };
+
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = false;
+    key = config.sops.secrets."syncthing/key".path;
+    cert = config.sops.secrets."syncthing/cert".path;
+    overrideDevices = true;
+    overrideFolders = true;
+    user = "nikodem";
+    group = "users";
+    dataDir = "${config.users.users.nikodem.home}/.local/share/syncthing";
+    configDir = "${config.users.users.nikodem.home}/.config/syncthing";
+    settings = {
+      devices = {
+        "pixel-6a".id = "L7IXLIC-DU3D4MJ-OVOXKBW-D5M2AW5-3JWOFF4-X43JUPT-JN3XWZL-2FNL6QT";
+        "tablet".id = "SCFNK0Z-UDF56C2-26ZS36D-A2PNTWO-K06IC55-0QGWCTN-MLNQBND-PXD8JQX";
+      };
+      folders = {
+        "obsidian" = {
+          path = "${config.users.users.nikodem.home}/vaults/main";
+          devices = ["pixel-6a" "tablet"];
+          copyOwnershipFromParent = true;
+          versioning = {
+            type = "simple";
+            params = {
+              keep = "5";
+            };
+          };
+        };
+      };
     };
   };
 }
