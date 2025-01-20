72c02c07d0d4325521b174545aac8b904809dd91
nm1 <nikodem.marek@softnet.com.pl>
2025-01-20 19:37:47 +0100

    feat(laptop): monitor setup for home

--- host/laptop/kanshi.nix
+++ host/laptop/kanshi.nix
@@ -19,6 +19,23 @@
           ];
         };
       }
+      {
+        profile = {
+          name = "home";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              mode = "1920x1080";
+              position = "0,0";
+            }
+            {
+              criteria = "HDMI-A-1";
+              mode = "2560x1440";
+              position = "-640,-1440";
+            }
+          ];
+        };
+      }
     ];
   };
 }
