c5ee1138e045b17afd5b3883e44b1de7e5053c9e
NikodemMarek <nikodemmarek11@gmail.com>
2025-03-01 18:15:57 +0100

    fix(tv): bad systemd file

--- host/tv/default.nix
+++ host/tv/default.nix
@@ -35,7 +35,10 @@
 
   services.cage = {
     enable = true;
-    program = "${lib.getExe pkgs.pulseaudio} set-default-sink alsa_output.pci-0000_00_03.0.hdmi-stereo ; ${lib.getExe pkgs.firefox} --new-instance --no-remote about:blank";
+    program = lib.getExe (pkgs.writeShellScriptBin "run" ''
+      ${lib.getExe pkgs.pulseaudio} set-default-sink alsa_output.pci-0000_00_03.0.hdmi-stereo
+      ${lib.getExe pkgs.firefox} --new-instance --no-remote about:blank
+    '');
     user = "root";
     extraArguments = ["-m" "last"];
   };
