8c1bbdf0e683d50e1c2522a9c3b7942d3685f6e5
nm1 <nikodem.marek@softnet.com.pl>
2025-03-20 17:33:33 +0100

    feat(LP-043): added syncthing and network definition

--- host/LP-043/default.nix
+++ host/LP-043/default.nix
@@ -84,4 +84,36 @@
       ];
     };
   };
+
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = false;
+    # key = config.sops.secrets."syncthing/key".path;
+    # cert = config.sops.secrets."syncthing/cert".path;
+    overrideDevices = true;
+    overrideFolders = true;
+    user = "nikodem";
+    group = "users";
+    dataDir = "${config.users.users.nikodem.home}/.local/share/syncthing";
+    configDir = "${config.users.users.nikodem.home}/.config/syncthing";
+    settings = {
+      devices = {
+        "pixel-6a".id = "L7IXLIC-DU3D4MJ-OVOXKBW-D5M2AW5-3JWOFF4-X43JUPT-JN3XWZL-2FNL6QT";
+        "tablet".id = "SCFNK0Z-UDF56C2-26ZS36D-A2PNTWO-K06IC55-0QGWCTN-MLNQBND-PXD8JQX";
+      };
+      folders = {
+        "obsidian" = {
+          path = "${config.users.users.nikodem.home}/vaults/main";
+          devices = ["pixel-6a" "tablet"];
+          copyOwnershipFromParent = true;
+          versioning = {
+            type = "simple";
+            params = {
+              keep = "5";
+            };
+          };
+        };
+      };
+    };
+  };
 }
