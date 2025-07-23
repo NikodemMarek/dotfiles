77a4ce3b233281649b52124dc24e373de696b7c4
Nikodem Marek <nikodem.marek@softnet.com.pl>
2025-07-23 12:38:31 +0200

    add basic disco without persistence

---
+++ host/features/disko/btrfs-no-persistence-swapfile.nix
@@ -0,0 +1,63 @@
+# run with: sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./btrfs-no-persistence-swapfile.nix --arg device '"eg. sda"' --arg swap 'number'
+{
+  device,
+  swap ? 4,
+  ...
+}: {
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/${device}";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            esp = {
+              priority = 1;
+              name = "boot";
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"];
+
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                  };
+
+                  "/swap" = {
+                    mountpoint = "/.swapvol";
+                    swap.swapfile.size = "${builtins.toString swap}G";
+                  };
+
+                  "/nix" = {
+                    mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
+                    mountpoint = "/nix";
+                  };
+
+                  "/home" = {
+                    mountOptions = ["subvol=home" "compress=zstd"];
+                    mountpoint = "/home";
+                  };
+                };
+
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
