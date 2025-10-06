da4f4736dbe819efef3bcc14e4501c49fb4b0fbb
NikodemMarek <62289991+NikodemMarek@users.noreply.github.com>
2025-10-06 11:18:05 +0200

    feat: basic immich config

--- host/server/immich.nix
+++ host/server/immich.nix
@@ -26,6 +26,21 @@ in {
         openFirewall = true;
         mediaLocation = "/mnt/photos";
         group = "photos";
+        settings = {
+          reverseGeocoding.enabled = true;
+          newVersionCheck.enabled = false;
+          server = {
+            externalDomain = "";
+          };
+          storageTemplate = {
+            enabled = true;
+            hashVerificationEnabled = true;
+            template = "{{y}}/{{MMM}}/{{dd}}/{{filename}}";
+          };
+        };
+        environment = {
+          IMMICH_TELEMETRY_INCLUDE = "all";
+        };
       };
 
       users.groups = {
