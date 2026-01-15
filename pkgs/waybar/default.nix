218046342c046797fe56da2b96a47ca0fa8173cf
NikodemMarek <62289991+NikodemMarek@users.noreply.github.com>
2026-01-15 20:13:46 +0100

    feat(waybar): add tailscale indicator

--- pkgs/waybar/default.nix
+++ pkgs/waybar/default.nix
@@ -4,7 +4,41 @@ pkgs.symlinkJoin {
   paths = [pkgs.waybar];
   buildInputs = [pkgs.makeWrapper];
   postBuild = let
-    extraPkgs = [pkgs.tailscale];
+    tailscale-status = pkgs.writeShellScriptBin "tailscale-status" ''
+      tailscale status --json | jq --unbuffered --compact-output -c '
+        [ .Peer[] | select(.ExitNode == true) ] as $node
+        | if ($node | length > 0) then
+            {
+              "text": $node[0].HostName,
+              "alt": $node[0].TailscaleIPs[0],
+              "tooltip": "\($node[0].HostName) (\($node[0].TailscaleIPs[0]))",
+              "class": "exit-node-on",
+              "percentage": 100
+            }
+          else
+            {
+              "text": "No Exit Node",
+              "alt": "none",
+              "tooltip": "Standard Routing",
+              "class": "exit-node-off",
+              "percentage": 0
+            }
+          end'
+    '';
+    tailscale-toggle = pkgs.writeShellScriptBin "tailscale-toggle" ''
+      STATUS=$(tailscale-status | jq .percentage)
+      if [ $STATUS -ge 50 ]; then
+        tailscale up --operator=$USER --accept-routes --exit-node=
+      else
+        EXIT_NODE_IP=$(host triss | awk '{print $4}')
+        tailscale up --operator=$USER --accept-routes --exit-node=$EXIT_NODE_IP --exit-node-allow-lan-access=true
+      fi
+    '';
+    extraPkgs = [
+      pkgs.tailscale
+      tailscale-status
+      tailscale-toggle
+    ];
   in ''
     wrapProgram $out/bin/waybar \
         --suffix PATH : ${pkgs.lib.strings.makeBinPath extraPkgs} \
