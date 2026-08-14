43087441e90248aee55c02c9bd9c9800e08614c9
NikodemMarek <62289991+NikodemMarek@users.noreply.github.com>
2026-08-14 14:53:35 +0200

    fix(alp): allow root login for the installation medium

--- host/alp/default.nix
+++ host/alp/default.nix
@@ -14,23 +14,24 @@
     # Generate the key in tailscale console, Settings > Keys.
     authKeyFile = ./key;
   };
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "no";
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
+  services.openssh.enable = true;
+
+  nix.settings.trusted-users = ["maintenance" "@wheel"];
+  users.users = {
+    root = {
+      password = lib.mkForce "nixos";
+      openssh.authorizedKeys.keyFiles = [
+        ../yenn/user_nikodem_ssh_id_ed25519.pub
+      ];
+    };
+    nixos = {
+      password = lib.mkForce "nixos";
+      openssh.authorizedKeys.keyFiles = [
+        ../yenn/user_nikodem_ssh_id_ed25519.pub
+      ];
     };
   };
 
-  nix.settings.trusted-users = ["maintenance" "@wheel"];
-  users.users.nixos = {
-    password = lib.mkForce "nixos";
-    openssh.authorizedKeys.keyFiles = [
-      ../yenn/user_nikodem_ssh_id_ed25519.pub
-    ];
-  };
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.supportedFilesystems = lib.mkForce ["btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs"];
+  boot.kernelPackages = pkgs.linuxPackages_6_18;
+  boot.supportedFilesystems = lib.mkForce ["btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" "zfs"];
 }
